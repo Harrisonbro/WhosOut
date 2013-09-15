c7ba6bdbbd2add9ee848acc82356ba4db14c3994
Harrison Brown <harrisonbro@gmail.com>
2013-09-15 18:20:08 +0100

    Started with an initial view

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ $f3=require('lib/base.php');
 $f3->config('config.ini');
 $f3->set('AUTOLOAD', 'app/controllers/'); 
 
-$f3->route('GET /', 'myTest->testMethod');
+$f3->route('GET /', 
+	function($f3) {
+		$f3->set('myvar', 'hello world'); 
+		$Template = new Template; 
+		echo $Template->render('mytempl.htm'); 
+	}
+);
 
 $f3->run();
